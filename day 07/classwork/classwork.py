1de288f628a31b5f358fbf0faf5a297bd9520f77
Rezi5 <rezidumbadze2010@gmail.com>
2025-03-03 05:25:53 +0400

    commit

---
+++ day 07/classwork/classwork.py
@@ -0,0 +1,32 @@
+for i in range(100, 1, -1):
+
+    print(i)
+
+for i in range(100, 1, -2):
+    print(i)
+            
+
+for i in range(1,101,2):
+    print(i)
+
+
+
+for i in range(250,501,10):
+    print(i)
+
+
+
+word = input("enter word:")
+
+if (word[0] == " _ "):
+     print(True)
+else:
+    print(False)
+
+
+num = int(input("whats num"))
+if num % 2 == 0:
+    for i in range(1,10):
+        print("yes")
+else:
+    print("no")
