686574d5af82c45d3f04d6a2e2944734646c725f
Rezi5 <rezidumbadze2010@gmail.com>
2025-03-18 16:40:43 +0400

    commit

--- day 11/homework/homework .py
+++ day 11/homework/homework .py
@@ -1,13 +1,15 @@
 #https://www.codewars.com/kata/5810085c533d69f4980001cf/train/python
-def calculator(x, y, op):
-    if (str(x).isdigit() or isinstance(x, float)) and (str(y).isdigit() or isinstance(y, float)):
-        x, y = float(x), float(y) 
-    else:
-        return "unknown value" 
-    
-    
-    
-    if op == "+":
+def calculator(x, y, op):   
+    if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        return "unknown value"
+
+    if op == '+':
         return x + y
-    elif op == "-":
+    elif op == '-':
         return x - y
+    elif op == '*':
+        return x * y
+    elif op == '/':
+        return x / y if y != 0 else "unknown value"
+    else:
+        return "unknown value"
